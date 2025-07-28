459d41b353acf8a32bf622fb7b2fbfe6c993aec1
mohdajeem <149220624+mohdajeem@users.noreply.github.com>
2025-07-29 05:16:57 +0530

    make changes to baseUrl for frontend

--- client/src/services/api.js
+++ client/src/services/api.js
@@ -1,35 +1,7 @@
-// import axios from 'axios';
-
-// // Use an environment variable for your API base URL
-// // const API_BASE_URL = process.env.REACT_APP_API_URL || 'http://localhost:5000/api/v1';
-
-// const API_BASE_URL = import.meta.env.VITE_API_URL || 'http://localhost:5000/api/v1';
-
-
-// const api = axios.create({
-//     baseURL: API_BASE_URL,
-// });
-
-// // Interceptor to add the JWT token to every outgoing request
-// api.interceptors.request.use(
-//     (config) => {
-//         const token = localStorage.getItem('token');
-//         if (token) {
-//             config.headers['Authorization'] = `Bearer ${token}`;
-//         }
-//         return config;
-//     },
-//     (error) => {
-//         return Promise.reject(error);
-//     }
-// );
-
-// export default api;
-
 import axios from 'axios';
 
 const api = axios.create({
-    baseURL: import.meta.env.VITE_API_URL,
+    baseURL: `${import.meta.env.VITE_API_URL}/api/v1`,
 });
 
 api.interceptors.request.use(
